f59e22f6481d4e5e529f37eb5da6f272e9cdf0f8
Alex Wilkinson <awilk901@gmail.com>
2019-12-10 21:54:50 -0600

    Turn off https redirection

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,20 +23,20 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) // Dependency Injection Container
         {
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        { // Middleware
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
